fd46169063876ed23bb127b50d4759502effeb3b
qingwave <isguory@gmail.com>
2023-11-22 15:13:00 +0800

    create project

---
+++ background.js
@@ -0,0 +1,107 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.get(['interval', 'restInterval'], function (data) {
+    const interval = data.interval || 60;
+    const restInterval = data.restInterval || 5;
+    chrome.storage.sync.set({
+      'interval': interval,
+      'restInterval': restInterval,
+    });
+    createReminder(interval, skipRest = true)
+
+    console.log(`Reminder onIntalled with interval ${interval}`)
+  })
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  if (namespace === 'sync') {
+    if (changes.interval) {
+      const newInterval = changes.interval.newValue;
+      console.log(`Interval changed to ${newInterval}`);
+
+      createReminder(newInterval, skipRest = true)
+    }
+  }
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm.name === 'reminder') {
+    chrome.storage.sync.get(['interval', 'skipTimes'], function (data) {
+      const interval = data.interval || 60;
+      const skipTimes = data.skipTimes || 0;
+
+      console.log('Reminder Trigger');
+
+      createNotification(interval, skipTimes)
+
+      const now = new Date();
+      chrome.storage.sync.set({ 'lastReminder': now.toLocaleTimeString() });
+    });
+  }
+});
+
+function createNotification(interval, skipTimes) {
+  let duration = interval;
+  if (skipTimes > 0) {
+    duration = interval * (skipTimes + 1);
+  }
+
+  chrome.notifications.clear('movemate', function () {
+    chrome.notifications.create("movemate", {
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: chrome.i18n.getMessage('messageTitle'),
+      message: `${chrome.i18n.getMessage('reminderContentPrefix')} ${duration} ${chrome.i18n.getMessage('reminderContentSuffix')}`,
+      buttons: [{ title: chrome.i18n.getMessage('skip'), iconUrl: 'icon.png' }]
+    });
+  });
+}
+
+function createReminder(interval, skipRest = false) {
+  chrome.storage.sync.get('restInterval', function (data) {
+    let duration = interval;
+    if (!skipRest) {
+      duration += data.restInterval;
+    }
+
+    chrome.alarms.clear('reminder', function () {
+      chrome.alarms.create('reminder', { delayInMinutes: duration });
+    });
+
+    const now = new Date();
+    const nextReminder = new Date(now.getTime() + duration * 60000);
+    console.log("next trigger", nextReminder, "skipping rest", skipRest)
+    chrome.storage.sync.set({ 'nextReminder': nextReminder.getTime() });
+  });
+}
+
+chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
+  console.log('Button clicked! Notification ID:', notificationId, 'Button Index:', buttonIndex);
+  if (notificationId === 'movemate' && buttonIndex === 0) {
+    console.log("user skip this rest")
+    chrome.notifications.clear(notificationId)
+
+    chrome.storage.sync.get('skipTimes', function (data) {
+      const skipTimes = data.skipTimes || 0;
+
+      chrome.storage.sync.set({ 'skipTimes': skipTimes + 1, 'userSetSkip': true });
+    });
+  }
+});
+
+chrome.notifications.onClosed.addListener(function (notificationId, byUser) {
+  if (notificationId === 'movemate') {
+    console.log("movemate notification closed", byUser)
+    chrome.storage.sync.get(['interval', 'skipTimes', 'userSetSkip'], function (data) {
+      const interval = data.interval;
+      const skipTimes = data.skipTimes || 0;
+      const userSetSkip = data.userSetSkip || false;
+
+      createReminder(interval, userSetSkip)
+      if (!userSetSkip && skipTimes > 0) {
+        chrome.storage.sync.set({ 'skipTimes': 0 });
+      }
+
+      chrome.storage.sync.remove('userSetSkip');
+    });
+  }
+});
---
+++ popup.js
@@ -0,0 +1,121 @@
+
+function setReminder(interval) {
+  chrome.storage.sync.set({ 'interval': parseInt(interval) }, function () {
+    console.log(`set reminder interval to ${interval}`);
+
+    const now = new Date();
+    const nextReminder = new Date(now.getTime() + interval * 60000);
+    setNextReminder(nextReminder.getTime())
+
+    chrome.notifications.create('setReminder', {
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: chrome.i18n.getMessage('messageTitle') + chrome.i18n.getMessage('setting'),
+      message: chrome.i18n.getMessage('successApplyMessage') + ` ${interval} ` + chrome.i18n.getMessage('minute') + `\n${chrome.i18n.getMessage('nextReminder')}: ${nextReminder.toLocaleTimeString()}`
+    });
+  });
+}
+
+document.querySelectorAll('[data-locale]').forEach(elem => {
+  elem.innerText = chrome.i18n.getMessage(elem.dataset.locale)
+})
+
+chrome.storage.sync.get(['interval', 'restInterval'], (data) => {
+  document.getElementById('interval').value = data.interval || 60;
+  document.getElementById('restInterval').value = data.restInterval || 5;
+})
+
+document.getElementById('reminderButton').addEventListener('click', function () {
+  const intervalInput = document.getElementById('interval');
+  const intervalValue = parseInt(intervalInput.value);
+
+  if (isNaN(intervalValue) || intervalValue < parseInt(intervalInput.min) || intervalValue % parseInt(intervalInput.step) !== 0) {
+    alert(`input must great than ${intervalInput.min}, step must be ${intervalInput.step}`);
+    return;
+  }
+
+  setReminder(intervalValue);
+});
+
+document.getElementById('restButton').addEventListener('click', function () {
+  console.log("xxxx")
+
+  const restInput = document.getElementById('restInterval');
+  const restValue = parseInt(restInput.value);
+
+  if (isNaN(restValue) || restValue < parseInt(restInput.min) || restValue > parseInt(restInput.max)) {
+    alert(`input must great than ${restInput.min}, less than ${restInput.max}`);
+    return;
+  }
+
+  chrome.storage.sync.set({ 'restInterval': restValue }, function () {
+    console.log(`set rest interval to ${restValue}`);
+    chrome.notifications.create('setRest', {
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: chrome.i18n.getMessage('messageTitle') + chrome.i18n.getMessage('setting'),
+      message: chrome.i18n.getMessage('setRestIntervalContent') + ` ${restValue} ` + chrome.i18n.getMessage('minute')
+    });
+  })
+});
+
+document.getElementById('testReminder').addEventListener('click', function () {
+  chrome.notifications.create('testNotification', {
+    type: 'basic',
+    iconUrl: 'icon.png',
+    title: chrome.i18n.getMessage('messageTitle'),
+    message: chrome.i18n.getMessage('testMessage')
+  });
+});
+
+function setNextReminder(next) {
+  const now = new Date();
+  const minutesRemaining = Math.round((next - now.getTime()) / 60000);
+  let text = `${minutesRemaining} ${chrome.i18n.getMessage('minutesLater')}`
+  if (minutesRemaining < 1) {
+    text = chrome.i18n.getMessage('remindSoon')
+  }
+  document.getElementById('nextReminderTime').textContent = text;
+}
+
+function setLastReminder(val) {
+  document.getElementById('lastReminderTime').textContent = val;
+}
+
+function setSkipTimes(val) {
+  document.getElementById('skipTimes').textContent = val;
+}
+
+chrome.storage.sync.get(['lastReminder', 'nextReminder', 'skipTimes'], function (data) {
+  const lastReminderTime = data.lastReminder || 'NaN';
+  const nextReminder = data.nextReminder;
+  const skipTimes = data.skipTimes || 0;
+
+  setLastReminder(lastReminderTime);
+  setNextReminder(nextReminder);
+  setSkipTimes(skipTimes);
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  if (namespace === 'sync') {
+    if (changes.lastReminderTime) {
+      const newVal = changes.lastReminderTime.newValue;
+      setLastReminder(newVal);
+    }
+    if (changes.nextReminder) {
+      const newVal = changes.nextReminder.newValue;
+      setNextReminder(newVal);
+    }
+    if (changes.skipTimes) {
+      const newVal = changes.skipTimes.newValue;
+      setSkipTimes(newVal);
+    }
+  }
+});
+
+setInterval(function(){
+  chrome.storage.sync.get('nextReminder', function (data) {
+    const nextReminder = data.nextReminder;
+    setNextReminder(nextReminder);
+  });
+}, 30000)
